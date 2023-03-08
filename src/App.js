4c7a8bfa24bcc60dd8a00d379e3e20eaf4ec2518
Evaristo <francisco.evaristo.garcia@gmail.com>
2023-03-07 21:33:57 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -177,6 +177,7 @@ const Component = ({
         origin={false}
         resizable
         draggable
+        snappable
         bounds={{left: 0, top: 0, bottom: 0, right: 0, position: "css" }}
         onResizeStart={e => {
             e.setFixedDirection([0, 0]);
